a3a9d203942729480f351bd03545d7ccdde28c93
Georg G <nilsding@nilsding.org>
2016-11-01 22:05:56 +0100

    Update to Swift 3

--- BeatsTime/main.swift
+++ BeatsTime/main.swift
@@ -11,19 +11,19 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusItem: NSStatusItem!
-    var timer: NSTimer!
+    var timer: Timer!
     var precision: Int! = 0
     var precisionItems = Array<NSMenuItem>()
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         // initialize menu
         let menu = NSMenu()
         let menuPrecision = NSMenu()
         let menuPrecisionItem = NSMenuItem(title: "Precision", action: nil, keyEquivalent: "")
         menuPrecisionItem.submenu = menuPrecision
         
-        0.stride(to: 4, by: 1).forEach { (num: Int) -> () in
-            precisionItems.append(NSMenuItem(title: String(num), action: "setPrecision:", keyEquivalent: ""))
+        stride(from: 0, to: 4, by: 1).forEach { (num: Int) -> () in
+            precisionItems.append(NSMenuItem(title: String(num), action: #selector(AppDelegate.setPrecision(_:)), keyEquivalent: ""))
         }
         precisionItems.first!.state = NSOnState
         
@@ -32,44 +32,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         
         menu.addItem(menuPrecisionItem)
-        menu.addItem(NSMenuItem.separatorItem())
-        menu.addItem(NSMenuItem(title: "Quit", action: "terminate:", keyEquivalent: "q"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.shared().terminate(_:)), keyEquivalent: "q"))
         
         // initialize statusItem
-        statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+        statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
         statusItem.title = "@000"
         statusItem.menu = menu
         
         // initialize update timer and add it to the run loop
-        timer = NSTimer(timeInterval: 1, target: self, selector: "update", userInfo: nil, repeats: true)
-        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+        timer = Timer(timeInterval: 1, target: self, selector: #selector(AppDelegate.update), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: RunLoopMode.commonModes)
         update()
     }
     
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         timer.invalidate()
-        NSStatusBar.systemStatusBar().removeStatusItem(statusItem)
+        NSStatusBar.system().removeStatusItem(statusItem)
     }
     
-    func calculateBeats(aDate: NSDate) -> Double {
+    func calculateBeats(_ aDate: Date) -> Double {
         // this feels really hack-ish
-        let fmt = NSDateFormatter()
-        fmt.timeZone = NSTimeZone(name: "UTC")
+        let fmt = DateFormatter()
+        fmt.timeZone = TimeZone(identifier: "UTC")
         fmt.dateFormat = "HH"
-        let hours = (Int(fmt.stringFromDate(aDate))! + 1) % 24
+        let hours = (Int(fmt.string(from: aDate))! + 1) % 24
         fmt.dateFormat = "mm"
-        let minutes = Int(fmt.stringFromDate(aDate))!
+        let minutes = Int(fmt.string(from: aDate))!
         fmt.dateFormat = "ss"
-        let seconds = Int(fmt.stringFromDate(aDate))!
+        let seconds = Int(fmt.string(from: aDate))!
         return Double(seconds + (minutes * 60) + (hours * 3600)) / 86.4
     }
     
     func update() {
-        let beats = calculateBeats(NSDate())
+        let beats = calculateBeats(Date())
         statusItem.title = NSString(format: "@%.*f", precision, beats) as String
     }
     
-    func setPrecision(sender: NSMenuItem) {
+    func setPrecision(_ sender: NSMenuItem) {
         precision = Int(sender.title)
         precisionItems.forEach { (item: NSMenuItem) -> () in
             item.state = NSOffState
@@ -81,6 +81,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 autoreleasepool {
     let ad = AppDelegate()
-    NSApplication.sharedApplication().delegate = ad
-    NSApplication.sharedApplication().run()
+    NSApplication.shared().delegate = ad
+    NSApplication.shared().run()
 }
